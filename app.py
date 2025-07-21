0c892a74ea98a65c129bb31b5f5c53f3d4b5c1a0
Ubuntu <ubuntu@ip-172-31-9-211.ap-south-1.compute.internal>
2025-07-21 20:37:22 +0000

    9th commit

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Vishnu is The king of devops!!Hey .... someone please give him a job 😭"
+    return "🚀 Vishnu is The Master of devops!!Hey .... someone please give him a job 😭"
 if __name__ == '__main__':     
     app.run(host='0.0.0.0', port=5000)
